bdb171183408d769124cd8609a2ca8142bd2c577
Simon-Pk <ap.va.14@inbox.ru>
2020-06-30 11:10:47 +0300

    added function arr

--- 9.c
+++ 9.c
@@ -1,1 +1,24 @@
-
+#include <stdio.h>
+#include <locale.h>
+int arr(int **a,int n){
+    printf("¬ведите элементы матрицы\n");
+a=(int**) malloc(sizeof(int*)*n);
+for(int i=0;i<n;i++){
+a[i]=(int*) malloc(sizeof(int)*(n+1));
+a[i][n]=0;
+for(int j=0;j<n;j++){
+printf("a[%d][%d]=",i,j);
+scanf("%d",&a[i][j]);
+a[i][n]+=a[i][j];
+}
+}
+return a;
+}
+int main()
+{
+setlocale(LC_ALL,"Russian");
+int n;
+printf("¬ведите пор€док матрицы\n");
+scanf("%d",&n);
+int **a=arr(a,n);
+}
